41d9f758b6e5b861048e7603f8eab8a2c99f37b6
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2022-04-12 20:52:40 -0300

    heranca.sql

---
+++ codigos/heranca.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS heranca;
+
+CREATE DATABASE heranca;
+
+\c heranca;
+
+create table funcionario (
+     matricula int,
+     nome varchar,
+     data_nascimento date,
+     primary key(matricula)
+);
+
+ 
+create table gerente (
+     percentParticipacaLucro int,
+     telCel varchar
+) inherits (funcionario);
+
+
+insert into funcionario values (2000, 'Maria', '02/02/1980'); -- Os dados são inseridos somente na tabela funcionários e não na tabela gerente.
+
+
+insert into gerente values (1000, 'Hesley', '01/01/1975', 10, '99999999'); -- Ao inserir um gerente, automaticamente os atributos herdados (matricula, nome, dataNascimento) são inseridos também na tabela de funcionários.
+
+
+
+
+
+
+
