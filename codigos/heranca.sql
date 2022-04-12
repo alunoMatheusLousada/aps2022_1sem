90ec86347e4230222227364db72d2586d41f72c2
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2022-04-12 20:54:11 -0300

    heranca.sql

--- codigos/heranca.sql
+++ codigos/heranca.sql
@@ -24,7 +24,9 @@ insert into funcionario values (2000, 'Maria', '02/02/1980'); -- Os dados são i
 insert into gerente values (1000, 'Hesley', '01/01/1975', 10, '99999999'); -- Ao inserir um gerente, automaticamente os atributos herdados (matricula, nome, dataNascimento) são inseridos também na tabela de funcionários.
 
 
-
+-- heranca=# SELECT * FROM funcionario ;
+-- heranca=# SELECT * FROM gerente ;
+-- heranca=# SELECT * FROM only funcionario;
 
 
 
